5ecbe1a5afad0624a44ff60fe8b79a43427fe000
Ahukko8 <ahukko88@gmail.com>
2024-11-19 12:51:01 +0500

    first commit

---
+++ src/app/(shop)/index.tsx
@@ -0,0 +1,32 @@
+import { PRODUCTS } from "@/assets/products";
+import { ListHeader } from "@/src/components/list-header";
+import { ProductListItem } from "@/src/components/product-list-item";
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <FlatList
+        data={PRODUCTS}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={2}
+        ListHeaderComponent={ListHeader}
+        contentContainerStyle={styles.flatListContent}
+        columnWrapperStyle={styles.flatListColumn}
+        style={{ paddingHorizontal: 10, paddingVertical: 5 }}
+      />
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  flatListContent: {
+    paddingBottom: 20,
+  },
+  flatListColumn: {
+    justifyContent: "space-between",
+  },
+});
---
+++ src/app/_layout.tsx
@@ -0,0 +1,32 @@
+import { Stack } from "expo-router";
+import { ToastProvider } from "react-native-toast-notifications";
+
+export default function RootLayout() {
+  return (
+   <ToastProvider>
+     <Stack>
+      <Stack.Screen
+        name="(shop)"
+        options={{ headerShown: false, title: "Shop" }}
+      />
+      <Stack.Screen
+        name="categories"
+        options={{ headerShown: false, title: "Categories" }}
+      />
+      <Stack.Screen
+        name="products"
+        options={{ headerShown: false, title: "Products" }}
+      />
+      <Stack.Screen
+        name="cart"
+        options={{ presentation: "modal", title: "Shopping Cart" }}
+      />
+      <Stack.Screen
+        name="auth"
+        options={{ headerShown: true, title: "User Authentication" }}
+      />
+    </Stack>
+    
+   </ToastProvider>
+  );
+}
